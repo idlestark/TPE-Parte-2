38d222a6ae3c6fe6b68ecc74a8a7a95480ab60f6
Iñaki <inaki.seb@gmail.com>
2022-11-14 23:07:37 -0300

    Más tweaks

--- api-router.php
+++ api-router.php
@@ -1,15 +1,13 @@
 <?php
 require_once './libs/Router.php';
 require_once './app/controllers/prints-api.controller.php';
-
-// crea el router
+ 
 $router = new Router();
 
-// defina la tabla de ruteo
+
 $router->addRoute('prints', 'GET', 'PrintApiController', 'getPrints');
 $router->addRoute('prints/:ID', 'GET', 'PrintApiController', 'getPrint');
 $router->addRoute('prints/:ID', 'DELETE', 'PrintApiController', 'deletePrint');
 $router->addRoute('prints', 'POST', 'PrintApiController', 'insertPrint'); 
 
-// ejecuta la ruta (sea cual sea)
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
